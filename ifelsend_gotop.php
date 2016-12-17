bfbdff1d05a552d2f50b13bbb7e6f985d6521d1d
naodai naodai <naodai naodai>
2016-12-17 17:26:47 +0800

    提交wp后调整相关代码

--- ifelsend_gotop.php
+++ ifelsend_gotop.php
@@ -1,32 +1,31 @@
 <?php
 /**
- * @package Go_Top
- * @version 0.1
- */ 
+ * @package IfElseNd Go Top
+ * @version 1.0
+ */
 /*
-Plugin Name: GoTop 
+Plugin Name: IfElseNd Go Top
 Plugin URI: http://ifelsend.com/ 
-Description: If Page Scroll, well downer right of your screen on every page.
-Author: Naodai [zhao_brain@163.com]
-Version: 0.1 
+Description: If Page Scroll, well downer right of your screen on every page. 
+Author: Naodai
+Version: 1.0
 Author URI: http://ifelsend.com/
-*/
-function gotop() {
-	$lyrics = '<div id="toolBackTo" class="back-to" style="right: 210px; ">
-<a class="backtotop" href="#top">返回顶部
-<img id="back-tip" src="http://ifelsend.com/blog/wp-content/plugins/ifelsend_gotop/back-tip.png" class="back-tip">
-</a>
-</div>';
-	return wptexturize ( $lyrics );
-}
-function echoGotop() {
-	$chosen = gotop();
-	echo $chosen;
-}
-add_action ( 'wp_footer', 'echoGotop' );
+ */
+
+function ifelsend_gotop() {
+    $ifelsend_gotop_string = '<div id="toolBackTo" class="back-to" style="right: 210px; ">
+        <a class="backtotop" href="#top">返回顶部
+        <img id="back-tip" src="'.plugin_dir_url( __FILE__ ).'back-tip.png" class="back-tip">
+        </a>
+        </div>';
+    echo wptexturize ( $ifelsend_gotop_string );
+}
+
+add_action ( 'wp_footer', 'ifelsend_gotop' );
+
 // We need some CSS to position the paragraph
-function gotopJsCss() {	
-echo "
+function ifelsend_gotop_jscss() {	
+    echo "
 <style type='text/css'>
 .back-to {
 position: fixed;
@@ -41,12 +40,13 @@ display:none;
 .back-to .backtotop:hover {
 background-color: #333;
 background-position: 8px 13px;
-}.back-to .backtotop{
+}
+.back-to .backtotop{
 float: left;
 display: block;
 width: 50px;
 height: 50px;
-background: #666 url(http://ifelsend.com/blog/wp-content/plugins/ifelsend_gotop/btt.png) 8px -57px no-repeat;
+background: #666 url(".plugin_dir_url( __FILE__ )."btt.png) 8px -57px no-repeat;
 margin-bottom: 15px;
 outline: 0 none;
 text-indent: -9999em;
@@ -55,66 +55,92 @@ text-indent: -9999em;
 -webkit-border-radius: 4px;
 border-radius: 4px;
 position: relative;
-}.back-to .backtotop .back-tip {
+}
+.back-to .backtotop .back-tip {
 position: absolute;
 visibility: hidden;
-top: -31px;left: -10px;
+top: -31px;
+left: -10px;
 max-width: 69px;
 }
 </style>
 <script type='text/javascript'>
-/** * 回到页面顶部
+/**
+ * 回到页面顶部
  * @param acceleration 加速度
  * @param time 时间间隔 (毫秒)
- **/function goTop(acceleration, time) {
-	acceleration = acceleration || 0.1;
-	time = time || 16;
-	var x1 = 0;
-	var y1 = 0;
-	var x2 = 0;
-	var y2 = 0;
-	var x3 = 0;
-	var y3 = 0;
-	if (document.documentElement) {
-		x1 = document.documentElement.scrollLeft || 0;
-		y1 = document.documentElement.scrollTop || 0;
-	}
-	if (document.body) {
-		x2 = document.body.scrollLeft || 0;
-		y2 = document.body.scrollTop || 0;
-	}
-	var x3 = window.scrollX || 0;
-	var y3 = window.scrollY || 0; 
-	// 滚动条到页面顶部的水平距离
-	var x = Math.max(x1, Math.max(x2, x3));
-	// 滚动条到页面顶部的垂直距离
-	var y = Math.max(y1, Math.max(y2, y3));
-	// 滚动距离 = 目前距离 / 速度, 因为距离原来越小, 速度是大于 1 的数, 所以滚动距离会越来越小
-	var speed = 1 + acceleration;
-	window.scrollTo(Math.floor(x / speed), Math.floor(y / speed));
-	// 如果距离不为零, 继续调用迭代本函数
-	if(x > 0 || y > 0) {
-		var invokeFunction = 'goTop(' + acceleration + ', ' + time + ')';
-		window.setTimeout(invokeFunction, time);
-	}
+ **/
+function goTop(acceleration, time) {
+    acceleration = acceleration || 0.1;
+    time = time || 16;
+
+    var x1 = 0;
+    var y1 = 0;
+    var x2 = 0;
+    var y2 = 0;
+    var x3 = 0;
+    var y3 = 0;
+
+    if (document.documentElement) {
+        x1 = document.documentElement.scrollLeft || 0;
+        y1 = document.documentElement.scrollTop || 0;
+    }
+    if (document.body) {
+        x2 = document.body.scrollLeft || 0;
+        y2 = document.body.scrollTop || 0;
+    }
+    var x3 = window.scrollX || 0;
+    var y3 = window.scrollY || 0;
+
+    // 滚动条到页面顶部的水平距离
+    var x = Math.max(x1, Math.max(x2, x3));
+    // 滚动条到页面顶部的垂直距离
+    var y = Math.max(y1, Math.max(y2, y3));
+
+    // 滚动距离 = 目前距离 / 速度, 因为距离原来越小, 速度是大于 1 的数, 所以滚动距离会越来越小
+    var speed = 1 + acceleration;
+    window.scrollTo(Math.floor(x / speed), Math.floor(y / speed));
+
+    // 如果距离不为零, 继续调用迭代本函数
+    if(x > 0 || y > 0) {
+        var invokeFunction = 'goTop(' + acceleration + ', ' + time + ')';
+        window.setTimeout(invokeFunction, time);
+    }
 }
-	window.onload = function(){
-		topShow();
-		document.getElementById('toolBackTo').onmouseover = function(){ document.getElementById('back-tip').style.visibility='visible';};
-		document.getElementById('toolBackTo').onmouseout = function(){ document.getElementById('back-tip').style.visibility='hidden';};
-		document.getElementById('toolBackTo').onclick = function(){goTop();return false;};
-		window.onscroll = topShow;
-	};
-	function topShow(){
-	    	var bt = document.getElementById('toolBackTo'),st = document.body.scrollTop,show = (st>30);
-			if( show ){
-				bt.style.display='block';
-			}else{
-				bt.style.display='none';
-			}
-		}
+function addEvent(obj,type,handle){
+    try{  // Chrome、FireFox、Opera、Safari、IE9.0及其以上版本
+        obj.addEventListener(type,handle,false);
+    }catch(e){
+        try{  // IE8.0及其以下版本
+            obj.attachEvent('on' + type,handle);
+        }catch(e){  // 早期浏览器
+            obj['on' + type] = handle;
+        }
+    }
+}
+
+addEvent(window,'load',loaded);
+
+function loaded(){
+    topShow();
+    var toolBackTo = document.getElementById('toolBackTo');    
+    addEvent(toolBackTo,'mouseover',function(){ document.getElementById('back-tip').style.visibility='visible';});
+    addEvent(toolBackTo,'mouseout',function(){ document.getElementById('back-tip').style.visibility='hidden';});
+    addEvent(toolBackTo,'click',function(){goTop();return false;});
+    addEvent(window,'scroll',topShow);
+}
+function topShow(){
+        var bt = document.getElementById('toolBackTo'),st = document.documentElement.scrollTop == 0 ? document.body.scrollTop : document.documentElement.scrollTop,show = (st>30);
+        if( show ){
+            bt.style.display='block';
+        }else{
+            bt.style.display='none';
+        }
+    }
 </script>
-	";
+    ";
 }
-add_action ( 'wp_footer', 'gotopJsCss' );
+
+add_action ( 'wp_footer', 'ifelsend_gotop_jscss' );
+
 ?>
---
+++ readme.txt
@@ -0,0 +1,44 @@
+=== IfElseNd Go Top ===
+Contributors: naodai
+Donate link: http://ifelsend.com/donate/
+Tags: back, back to top, navigation, navigate, page, scroll, scroll to top, scroll top, scroll up, top, up
+Requires at least: 1.0
+Tested up to: 1.0
+Stable tag: 1.0
+License: GPLv3
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+IfElseNd Go Top plugin allows the visitor to easily scroll back to the top of the page.
+
+== Description ==
+IfElseNd Go Top plugin allows the visitor to easily scroll back to the top of the page.
+
+== Installation ==
+
+1. Click Plugins/Add New from the WordPress admin panel
+1. Search for "IfElseNd Go Top" and install
+
+-or-
+
+1. Download the .zip package
+1. Unzip into the subdirectory 'ifelsend_gotop' within your local WordPress plugins directory
+1. Refresh plugin page and activate plugin
+
+== Frequently Asked Questions ==
+
+Please email to zhao_brain@163.com, We will be in the shortest possible time to help you solve.
+
+== Screenshots ==
+
+1. This screen shot description corresponds to screenshot-1.(png|jpg|jpeg|gif). Note that the screenshot is taken from
+the /assets directory or the directory that contains the stable readme.txt (tags or trunk). Screenshots in the /assets 
+directory take precedence. For example, `/assets/screenshot-1.png` would win over `/tags/4.3/screenshot-1.png` 
+(or jpg, jpeg, gif).
+2. This is the second screen shot
+
+== Changelog ==
+An upgraded version is on its way.
+
+
+== Upgrade Notice ==
+Will update it pretty soon.
