48251d6451bb23ea43386f5bfe606ad9d55fbe89
naodai naodai <naodai naodai>
2017-02-10 11:52:38 +0800

    1.0.1版本修改

--- ifelsend_gotop.php
+++ ifelsend_gotop.php
@@ -10,22 +10,22 @@ Description: If Page Scroll, well downer right of your screen on every page.
 Author: Naodai
 Version: 1.0
 Author URI: http://ifelsend.com/
- */
+*/
 
 function ifelsend_gotop() {
-    $ifelsend_gotop_string = '<div id="toolBackTo" class="back-to" style="right: 210px; ">
-        <a class="backtotop" href="#top">返回顶部
-        <img id="back-tip" src="'.plugin_dir_url( __FILE__ ).'back-tip.png" class="back-tip">
-        </a>
-        </div>';
-    echo wptexturize ( $ifelsend_gotop_string );
+	$ifelsend_gotop_string = '<div id="toolBackTo" class="back-to" style="right: 210px; ">
+<a class="backtotop" href="javascript:;">返回顶部
+<img id="back-tip" src="'.plugin_dir_url( __FILE__ ).'back-tip.png" class="back-tip">
+</a>
+</div>';
+	echo wptexturize ( $ifelsend_gotop_string );
 }
 
 add_action ( 'wp_footer', 'ifelsend_gotop' );
 
 // We need some CSS to position the paragraph
 function ifelsend_gotop_jscss() {	
-    echo "
+echo "
 <style type='text/css'>
 .back-to {
 position: fixed;
@@ -71,41 +71,41 @@ max-width: 69px;
  * @param time 时间间隔 (毫秒)
  **/
 function goTop(acceleration, time) {
-    acceleration = acceleration || 0.1;
-    time = time || 16;
-
-    var x1 = 0;
-    var y1 = 0;
-    var x2 = 0;
-    var y2 = 0;
-    var x3 = 0;
-    var y3 = 0;
-
-    if (document.documentElement) {
-        x1 = document.documentElement.scrollLeft || 0;
-        y1 = document.documentElement.scrollTop || 0;
-    }
-    if (document.body) {
-        x2 = document.body.scrollLeft || 0;
-        y2 = document.body.scrollTop || 0;
-    }
-    var x3 = window.scrollX || 0;
-    var y3 = window.scrollY || 0;
-
-    // 滚动条到页面顶部的水平距离
-    var x = Math.max(x1, Math.max(x2, x3));
-    // 滚动条到页面顶部的垂直距离
-    var y = Math.max(y1, Math.max(y2, y3));
-
-    // 滚动距离 = 目前距离 / 速度, 因为距离原来越小, 速度是大于 1 的数, 所以滚动距离会越来越小
-    var speed = 1 + acceleration;
-    window.scrollTo(Math.floor(x / speed), Math.floor(y / speed));
-
-    // 如果距离不为零, 继续调用迭代本函数
-    if(x > 0 || y > 0) {
-        var invokeFunction = 'goTop(' + acceleration + ', ' + time + ')';
-        window.setTimeout(invokeFunction, time);
-    }
+	acceleration = acceleration || 0.1;
+	time = time || 16;
+ 
+	var x1 = 0;
+	var y1 = 0;
+	var x2 = 0;
+	var y2 = 0;
+	var x3 = 0;
+	var y3 = 0;
+ 
+	if (document.documentElement) {
+		x1 = document.documentElement.scrollLeft || 0;
+		y1 = document.documentElement.scrollTop || 0;
+	}
+	if (document.body) {
+		x2 = document.body.scrollLeft || 0;
+		y2 = document.body.scrollTop || 0;
+	}
+	var x3 = window.scrollX || 0;
+	var y3 = window.scrollY || 0;
+ 
+	// 滚动条到页面顶部的水平距离
+	var x = Math.max(x1, Math.max(x2, x3));
+	// 滚动条到页面顶部的垂直距离
+	var y = Math.max(y1, Math.max(y2, y3));
+ 
+	// 滚动距离 = 目前距离 / 速度, 因为距离原来越小, 速度是大于 1 的数, 所以滚动距离会越来越小
+	var speed = 1 + acceleration;
+	window.scrollTo(Math.floor(x / speed), Math.floor(y / speed));
+ 
+	// 如果距离不为零, 继续调用迭代本函数
+	if(x > 0 || y > 0) {
+		var invokeFunction = 'goTop(' + acceleration + ', ' + time + ')';
+		window.setTimeout(invokeFunction, time);
+	}
 }
 function addEvent(obj,type,handle){
     try{  // Chrome、FireFox、Opera、Safari、IE9.0及其以上版本
@@ -138,7 +138,7 @@ function topShow(){
         }
     }
 </script>
-    ";
+	";
 }
 
 add_action ( 'wp_footer', 'ifelsend_gotop_jscss' );
--- readme.txt
+++ readme.txt
@@ -1,17 +1,21 @@
 === IfElseNd Go Top ===
 Contributors: naodai
-Donate link: http://ifelsend.com/donate/
+Donate link: http://ifelsend.com/blog/ifelsend-go-top/#donate
 Tags: back, back to top, navigation, navigate, page, scroll, scroll to top, scroll top, scroll up, top, up
 Requires at least: 1.0
-Tested up to: 1.0
-Stable tag: 1.0
+Tested up to: 4.7
+Stable tag: 1.0.1
 License: GPLv3
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 
 IfElseNd Go Top plugin allows the visitor to easily scroll back to the top of the page.
 
+IfElseNd Go Top 插件帮助访客快速的返回到页面顶部.
+
 == Description ==
 IfElseNd Go Top plugin allows the visitor to easily scroll back to the top of the page.
+
+IfElseNd Go Top 插件帮助访客快速的返回到页面顶部.
 
 == Installation ==
 
@@ -28,16 +32,20 @@ IfElseNd Go Top plugin allows the visitor to easily scroll back to the top of th
 
 Please email to zhao_brain@163.com, We will be in the shortest possible time to help you solve.
 
+如果你在使用过程中遇到任何问题,请联系 zhao_brain@163.com 或 访问 http://ifelsend.com/blog/ifelsend-go-top 留言,我会很快回复您的.
+
 == Screenshots ==
 
-1. This screen shot description corresponds to screenshot-1.(png|jpg|jpeg|gif). Note that the screenshot is taken from
-the /assets directory or the directory that contains the stable readme.txt (tags or trunk). Screenshots in the /assets 
-directory take precedence. For example, `/assets/screenshot-1.png` would win over `/tags/4.3/screenshot-1.png` 
-(or jpg, jpeg, gif).
-2. This is the second screen shot
+1. Show effect chart(页面滚动后,插件展示效果)
+2. Mouse over show effect chart(鼠标浮上后的效果)
 
 == Changelog ==
-An upgraded version is on its way.
+
+= 1.0.1 = 
+*Release Date - 10 February, 2017*
+
+* fix a href address(修复返回顶部a连接的herf值,让js函数生效)
+* add Chinese description(增加中文介绍) 
 
 
 == Upgrade Notice ==
